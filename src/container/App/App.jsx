96a24d2c55fe575e30349131886ce470be520ebc
Hatter <vu.nht00@gmail.com>
2023-09-13 21:08:37 +0700

    first version

--- src/container/App/App.jsx
+++ src/container/App/App.jsx
@@ -1,132 +1,165 @@
-import { useState, useMemo, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [ displayNavbarItems, setDisplayNavbarItems ] =  useState(window.innerWidth >= 992);
+  const [ isBigWidth, setIsBigWidth ] = useState(window.innerWidth >= 992);
   const [ displaySidebar, setDisplaySidebar ] = useState(false);
   useEffect(() => {
-    window.addEventListener('resize', () => setDisplayNavbarItems(window.innerWidth >= 992));
+    window.addEventListener('resize', () => setIsBigWidth(window.innerWidth >= 992));
   }, []);
 
   return (
     <div className="App">
-    <nav className="navbar">
-      <div className='md:container md:mx-auto mx-4 flex justify-between my-4' >
-        <a href="/" className='brand'>
-          <img src="src/assets/images/logo.svg" alt="brand" />
-        </a>
+      <nav className="navbar my-8 lg:mb-10 lg:mt-16">
+        <div className='md:container md:mx-auto mx-4 flex justify-between' >
+          <a href="/" className='brand'>
+            <img className="w-14 lg:w-20" src="src/assets/images/logo.svg" alt="brand" />
+          </a>
 
-        { displayNavbarItems && <div className='collapsed flex-1'>
-          <ul className='navitems flex justify-end items-center gap-6'>
-            <li className='nav-item'>
-              <a href='/' className='home nav-link'>
+          {isBigWidth && <div className='collapsed flex-1 flex items-center justify-end'>
+            <ul className='navitems flex justify-end items-center gap-6'>
+              <li className='nav-item'>
+                <a href='/' className='home nav-link'>
+                  Home
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a href='/' className='home nav-link'>
+                  New
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a href='/' className='home nav-link'>
+                  Popular
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a href='/' className='home nav-link'>
+                  Trending
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a href='/' className='home nav-link'>
+                Categories
+                </a>
+              </li>
+            </ul>
+          </div>}
+
+          <button className= "block lg:hidden" onClick={() => setDisplaySidebar(true)}>
+            <img src={'src/assets/images/icon-menu.svg'} />
+          </button>
+        </div>
+      </nav>
+
+      { displaySidebar && <div className='flex justify-end items-stretch fixed inset-0 bg-very-dark-blue/60'>
+        <div className="p-6 fixed w-[70%] max-w[250px] bg-off-white h-full">
+          <div className="flex flex-row items-center justify-end">
+            <button className= "block mb-16" onClick={() => setDisplaySidebar(false)}>
+              <img src={'src/assets/images/icon-menu-close.svg'} />
+            </button>
+          </div>
+          
+          <ul className='sidebar-items flex flex-col items-start justify-center gap-6 text-lg font-bold'>
+            <li className='side-item'>
+              <a href='/' className='home side-link'>
                 Home
               </a>
             </li>
-            <li className='nav-item'>
-              <a href='/' className='home nav-link'>
+            <li className='side-item'>
+              <a href='/' className='home side-link'>
                 New
               </a>
             </li>
-            <li className='nav-item'>
-              <a href='/' className='home nav-link'>
+            <li className='side-item'>
+              <a href='/' className='home side-link'>
                 Popular
               </a>
             </li>
-            <li className='nav-item'>
-              <a href='/' className='home nav-link'>
+            <li className='side-item'>
+              <a href='/' className='home side-link'>
                 Trending
               </a>
             </li>
-            <li className='nav-item'>
-              <a href='/' className='home nav-link'>
+            <li className='side-item'>
+              <a href='/' className='home side-link'>
               Categories
               </a>
             </li>
           </ul>
-        </div>}
-
-        <button className= "block" onClick={() => setDisplaySidebar(true)}>
-          <img src={'src/assets/images/icon-menu.svg'} />
-        </button>
-      </div>
-    </nav>
-
-    { displaySidebar && <div className='sidebar flex-1 flex flex-col fixed w-[60%]max-w[250px] p-4 top-0 right-0'>
-    <button className= "block justify-self-end" onClick={() => setDisplaySidebar(false)}>
-          <img src={'src/assets/images/close-icon-menu.svg'} />
-        </button>
-          <ul className='sidebar-items flex flex-col items-start justify-center gap-6'>
-            <li className='side-item'>
-              <a href='/' className='home side-link'>
-                Home
-              </a>
-            </li>
-            <li className='side-item'>
-              <a href='/' className='home side-link'>
-                New
-              </a>
-            </li>
-            <li className='side-item'>
-              <a href='/' className='home side-link'>
-                Popular
-              </a>
-            </li>
-            <li className='side-item'>
-              <a href='/' className='home side-link'>
-                Trending
-              </a>
-            </li>
-            <li className='side-item'>
-              <a href='/' className='home side-link'>
-              Categories
-              </a>
-            </li>
-          </ul>
-        </div>}
+        </div>
+      </div>}
   
-  <main>
-    <section className='big-content'>
-      <img src="/" alt="header-img" />
-      <h1>
-      The Bright Future of Web 3.0?
-      </h1>
-      <p className='content'>
-      We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
-But is it really fulfilling its promise?
-      </p>
+      <main className="mx-4 md:mx-auto md:container ">
+        <section className="flex flex-col gap-x-8 lg:flex-row">
+          <div className='big-content flex flex-col gap-6 mb-16 grow-[1.5]'>
+            <img src={isBigWidth ? "src/assets/images/image-web-3-desktop.jpg" : "src/assets/images/image-web-3-mobile.jpg"} alt="header-img" />
+            <div className="flex flex-col lg:flex-row gap-x-10">
+              <h1 className="font-extrabold text-5xl flex-1 text-very-dark-blue mb-8">
+              The Bright Future of Web 3.0?
+              </h1>
+              <div className="flex-1">
+                <p className='content mb-8'>
+                We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
+          But is it really fulfilling its promise?
+                </p>
 
-      <button 
-        className='' 
-        onClick={() => {location.href = '/'}}>
-      Read more
-      </button>
-    </section>
-  </main>
-
-
-  New 
-
-  Hydrogen VS Electric Cars
-  Will hydrogen-fueled cars ever catch up to EVs?
-
-  The Downsides of AI Artistry
-  What are the possible adverse effects of on-demand AI image generation?
-
-  Is VC Funding Drying Up?
-  Private funding by VC firms is down 50% YOY. We take a look at what that means.
-
-  01
-  Reviving Retro PCs
-  What happens when old PCs are given modern upgrades?
-
-  02
-  Top 10 Laptops of 2022
-  Our best picks for various needs and budgets.
-
-  03
-  The Growth of Gaming
-  How the pandemic has sparked fresh opportunities.
+                <button 
+                  className='text-off-white bg-soft-red py-4 px-0 inline-block w-[225px] uppercase font-bold text-xl tracking-widest' 
+                  onClick={() => {location.href = '/'}}>
+                Read more
+                </button>
+              </div>  
+            </div>
+            
+            
+          </div>
+          <div className="p-4 lg:p-8 bg-very-dark-blue text-off-white mb-16 grow-[1]">
+            <h2 className="text-soft-orange text-4xl font-extrabold mb-8">New</h2>
+            <div className="my-8 flex flex-col gap-4">
+              <h3 className="font-bold text-2xl">Hydrogen VS Electric Cars</h3>
+              <p className="text-grayish-blue">Will hydrogen-fueled cars ever catch up to EVs?</p>
+              <hr className="text-grayish-blue" />
+            </div>
+            <div className="my-8 flex flex-col gap-4">
+              <h3 className="font-bold text-2xl">The Downsides of AI Artistry</h3>
+              <p className="text-grayish-blue">What are the possible adverse effects of on-demand AI image generation?</p>
+              <hr className="text-grayish-blue" />
+            </div>
+            <div className="mt-8 mb-4 flex flex-col gap-4">
+              <h3 className="font-bold text-2xl">Is VC Funding Drying Up?</h3>
+              <p className="text-grayish-blue">Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+            </div>
+          </div>
+        </section>
+      
+        <section className="flex flex-col gap-y-10 gap-x-20 lg:flex-row lg:justify-between">
+          <div className="flex flex-row justify-center gap-6 max-w-[350px]">
+            <img className="w-[100px]" src="src/assets/images/image-retro-pcs.jpg" />
+            <div className="flex flex-col gap-y-4">
+              <h2 className="text-4xl font-extrabold text-grayish-blue">01</h2>
+              <h3 className="text-xl text-very-dark-blue font-extrabold">Reviving Retro PCs</h3>
+              <p>What happens when old PCs are given modern upgrades?</p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-6 max-w-[350px]">
+            <img className="w-[100px]" src="src/assets/images/image-top-laptops.jpg" />
+            <div className="flex flex-col gap-y-4">
+              <h2 className="text-4xl font-extrabold text-grayish-blue">02</h2>
+              <h3 className="text-xl text-very-dark-blue font-extrabold">Top 10 Laptops of 2022</h3>
+              <p>Our best picks for various needs and budgets.</p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-6 max-w-[350px]">
+            <img className="w-[100px]" src="src/assets/images/image-gaming-growth.jpg" />
+            <div className="flex flex-col gap-y-4">
+              <h2 className="text-4xl font-extrabold text-grayish-blue">03</h2>
+              <h3 className="text-xl text-very-dark-blue font-extrabold">The Growth of Gaming</h3>
+              <p>How the pandemic has sparked fresh opportunities.</p>
+            </div>
+          </div>
+        </section>
+      </main> 
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const { colors: defaultColors } = require('tailwindcss/defaultTheme');
+
+const colors = {
+    ...defaultColors,
+    ...{
+        'soft-orange': 'hsl(35, 77%, 62%)',
+        'soft-red': 'hsl(5, 85%, 63%)',
+        'off-white': 'hsl(36, 100%, 99%)',
+        'grayish-blue': 'hsl(233, 8%, 79%)',
+        'dark-grayish-blue': 'hsl(236, 13%, 42%)','very-dark-blue': 'hsl(240, 100%, 5%)'
+    },
+}
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    "colors": colors,
   },
   plugins: [],
 }
